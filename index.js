343be6528a3e9650227fc5bad3bb27d01cb7d4e9
guycreate <admin@domabo.com>
2019-08-14 16:35:09 -0500

    Removed dist from source control;   updated tsconfig

--- index.js
+++
@@ -1,6 +0,0 @@
-const IopaBot = require('./dist/index.js')
-exports = module.exports = IopaBot.default
-exports.default = IopaBot.default
-exports.constants = IopaBot.constants
-exports.BOT = IopaBot.BOT
-exports.useReactiveDialogs = IopaBot.useReactiveDialogs
